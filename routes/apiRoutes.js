6c1f49df50410be896c1931329e04af2a6eb6b8d
ajit.mishra <ajit.mishra@vivaconnec.co>
2024-03-07 15:44:00 +0530

    Clean Template

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,11 +3,7 @@ const apiController = require('../controllers/apiController');
 const {authenticateTokenUser,authenticateTokenAdmin} = require('../authenticate');
 const router = express.Router();
 
-// Define API routes
-// router.post('/add-item',authenticateTokenAdmin,apiController.addItem);
-// router.get('/list-items',authenticateTokenAdmin,apiController.listItem);
-// router.put('/update-items',authenticateTokenAdmin,apiController.updateItem);
-// router.delete('/delete-item',authenticateTokenAdmin,apiController.deleteItem);
+
 router.post('/reschedule-appointment',apiController.rescheduleAppointment)
 router.post('/schedule-appointment',apiController.schdeuleAppointment)
 router.delete('/delete-appointment',apiController.deleteAppointment)
